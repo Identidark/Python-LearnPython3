a8e8bc3edba7c554a0fa7c321fddc0fb86152353
rshang@SZ50026064 <rshang@abeam.com>
2019-07-16 09:14:57 +0800

    EscapeSequence

---
+++ Capter10_WhatsThat/EscapeSequence.py
@@ -0,0 +1,24 @@
+'''
+@message: 那是什么
+            转义字符
+@Author: Raynor.shang
+@since: 2019-07-16 09:02:14
+@lastTime: 2019-07-16 09:08:10
+@LastAuthor: Raynor.shang
+'''
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list;
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
