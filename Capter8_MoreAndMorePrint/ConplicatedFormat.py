9a8f600407e214af29d9ab1e69fc42b73070b4e0
rshang@SZ50026064 <rshang@abeam.com>
2019-07-16 08:39:35 +0800

    .format(parameters)

---
+++ Capter8_MoreAndMorePrint/ConplicatedFormat.py
@@ -0,0 +1,21 @@
+'''
+@message: 打印，打印
+            
+@Author: Raynor.shang
+@since: 2019-07-16 08:31:04
+@lastTime: 2019-07-16 08:35:54
+@LastAuthor: Raynor.shang
+'''
+
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Try your",
+    "Own text here",
+    "Maybe a poem",
+    "Or a song about fear"
+))
